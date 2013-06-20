70979f8189ab31f7bb01031bd5ffcec1e0ed4a3a
Chris <Cbeckerleg101@gmail.com>
2013-06-20 23:58:34 +0100

    Working code for square roots. Requires tests to be added

--- homework3/hw3.py
+++ homework3/hw3.py
@@ -6,3 +6,31 @@ Homework for solving newtons equation
 from numpy import *
 
 
+def solve(x0):
+      """
+      Doc-string text
+      """		
+          	
+      kmax=100
+      tol = 1.0e-14
+      
+      for k in range(kmax):
+              f,fp = target_func(x0)
+              
+              delta_x = f/fp    
+              x0 = x0-delta_x
+              print f,fp,x0,delta_x
+
+              if abs(delta_x/x0) < tol:
+                  break
+
+def target_func(x):
+    
+     f = x**2-x      
+     fp = 2.*x
+      
+     return f,fp
+
+
+
+
