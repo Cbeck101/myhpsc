49fa5ed185dce9c8baddab73fde81e316a72b3f0
Chris <Cbeckerleg101@gmail.com>
2013-06-20 13:17:08 +0100

    Prep for h/w

--- homework3/hw3.py
+++ homework3/hw3.py
@@ -1,21 +1,8 @@
-# $UWHPSC/codes/homework3/test_code.py 
-# To include in newton.py
+"""
+Homework for solving newtons equation
+"""
+
 
 from numpy import *
-# This algoritm works with varying succcess in differant regions. Implement a test for convergence to reduce the number of itterations once a solution has been found
 
-def sqrt2(x):
 
-	
-	s=1.
-	kmax=100
-	tol = 1.0e-14
-	for k in range(kmax):
-		s0=s
-		s=0.5*(s+ x/s)
-		delta_s = s-s0
-		print "s=%20.15f" % s
-		if abs(delta_s/x) < tol: # N.b error relative to x 
-
-		   break
-	print "After %g iterations s= %20.15f" % (k+1,s)
